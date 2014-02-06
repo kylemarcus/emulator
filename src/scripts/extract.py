62018b01730c5ff2a8f9ec580c138b9c4c23101f
Kyle Marcus <kmarcus2@f07n05.ccr.buffalo.edu>
2014-02-06 11:33:34 -0500

    combined extract.py and extract_data.py into one file

--- src/scripts/extract.py
+++ src/scripts/extract.py
@@ -4,29 +4,49 @@ import glob
 import argparse
 
 """
-Creates the file rohit.txt, which has information on pileheight records
+Creates output files needed for phase 1 of the emulator based on pileheight 
+records and the uncertain input list file.
 
+Outputs:
+phData.txt      - this was rohit.xt
+phMetaData.txt  - this was meta_data.txt
+
+Inputs:
 pileheights: /projects/gmfg/montserrat-2012/d_20cells/output_files_20cells
-
-rohit.txt contains:
- sample col row pileheight
+unvertain input list: /projects/gmfg/montserrat-2012/d_20cells/uncertain_input_list.txt
 """
 
 parser = argparse.ArgumentParser()
+parser.add_argument("uncertainInputList", help="Uncertain Input List file")
 parser.add_argument("inputDir", help="pileheight input directoy")
-parser.add_argument("outputDir", help="rohit.txt output directory")
+parser.add_argument("outputDir", help="output directory")
 args = parser.parse_args()
 
-with open(os.path.join(args.outputDir, 'rohit.txt'), 'w') as outputFp:
+# uncertain input list matrix
+u = []
+
+# read in uncertain_input_list.txt
+with open(args.uncertainInputList, 'r') as uilFp:
     
+    # read header
+    for i in range(6):
+        samples = uilFp.readline()
+    
+    # read in initial condition matrix
+    for i in range(int(samples)):
+        u += uilFp.readline().split()
+
+# write output files based on pileheight records
+with open(os.path.join(args.outputDir, 'phData.txt'), 'w') as dataFp, \
+     open(os.path.join(args.outputDir, 'phMetaData.txt'), 'w') as metaDataFp:
+    
+    # go through each pileheight record
     for phRecord in glob.iglob(os.path.join(args.inputDir, 'pileheight*')):
         
-        pileHeight = []
-
         filenumber = int(os.path.splitext(phRecord)[1][1:])
-        print phRecord, filenumber
+        print filenumber, phRecord
         
-        with open(phRecord,'r') as phRecordFp:
+        with open(phRecord, 'r') as phRecordFp:
 
             # read header
             Nx, xstart, xend = re.findall(r'[\d.]+', phRecordFp.readline())
@@ -34,13 +54,28 @@ with open(os.path.join(args.outputDir, 'rohit.txt'), 'w') as outputFp:
             phRecordFp.readline()
             
             # read pileheight matrix
+            pileHeight = []
             for i in range(int(Ny)):
                 pileHeight += phRecordFp.readline().split()
             
-            # write to output file
+            # write to data file
             for i in range(int(Ny)):
                 for j in range(int(Nx)):
-                    outputFp.write( str(filenumber) + ',' + \
-                                    str(i+1) + ',' + \
-                                    str(j+1) + ',' + \
-                                    str(pileHeight[i*int(Nx)+j]) + '\n' )
+                    dataFp.write(str(filenumber) + ',' + \
+                                 str(i+1) + ',' + \
+                                 str(j+1) + ',' + \
+                                 pileHeight[i*int(Nx)+j] + '\n')
+            
+            # write to meta data file
+            i = (filenumber-1)*4
+            metaDataFp.write(str(filenumber) + ',' + \
+                             str(Nx) + ',' + \
+                             str(Ny) + ',' + \
+                             str(xstart) + ',' + \
+                             str(xend) + ',' + \
+                             str(ystart) + ',' + \
+                             str(yend) + ',' + \
+                             u[i] + ',' + \
+                             u[i+1] + ',' + \
+                             u[i+2] + ',' + \
+                             u[i+3] + '\n')
